9dbc3270bd0cc5f179b15ef16dd9f0eb34a86dbf
FeroanicDaGreat <RF-Laptop@outlook.com>
2026-05-22 13:34:07 +0530

    Initial commit of VB sample project

---
+++ Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Entries = New ListBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        CheckBox1 = New CheckBox()
+        Button4 = New Button()
+        Results = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(446, 221)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Add Entry"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(408, 184)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(408, 269)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Label"
+        ' 
+        ' Entries
+        ' 
+        Entries.FormattingEnabled = True
+        Entries.Location = New Point(570, 184)
+        Entries.Name = "Entries"
+        Entries.Size = New Size(180, 129)
+        Entries.TabIndex = 3
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(570, 359)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 4
+        Button2.Text = "Choose Entry"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(570, 319)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(112, 34)
+        Button3.TabIndex = 5
+        Button3.Text = "Remove Entry"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(626, 41)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(117, 29)
+        CheckBox1.TabIndex = 6
+        CheckBox1.Text = "Automate"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(626, 76)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(112, 34)
+        Button4.TabIndex = 7
+        Button4.Text = "Confirm"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Results
+        ' 
+        Results.FormattingEnabled = True
+        Results.Location = New Point(378, 41)
+        Results.Name = "Results"
+        Results.Size = New Size(180, 129)
+        Results.TabIndex = 8
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Results)
+        Controls.Add(Button4)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Entries)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Entries As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Results As ListBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Net.Security
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim usrI As String = TextBox1.Text
+        Label1.Text = "Added " & usrI
+        Entries.Items.Add(usrI)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Entries.SelectedItem IsNot Nothing Then
+            MessageBox.Show("You chose: " & Entries.SelectedItem.ToString())
+        Else
+            MessageBox.Show("Please select something first!")
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Entries.SelectedItem IsNot Nothing Then
+            Entries.Items.Remove(Entries.SelectedItem)
+        Else
+            MessageBox.Show("Please select something first!")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If CheckBox1.Checked Then
+            ' Sort the items alphabetically
+            Dim sortedList As New List(Of String)
+            For Each item As String In Entries.Items
+                sortedList.Add(item)
+            Next
+            sortedList.Sort()
+
+            ' Clear and re-add sorted items
+            Entries.Items.Clear()
+            For Each item As String In sortedList
+                Entries.Items.Add(item)
+            Next
+
+            MessageBox.Show("Entries sorted alphabetically")
+        Else
+            MessageBox.Show("Entries left Default")
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim searchText As String = TextBox1.Text.ToLower()
+
+        ' Clear current items
+        Results.Items.Clear()
+
+        ' Re-add only items that match the search
+        For Each item As String In Entries.Items
+            If item.ToLower().StartsWith(searchText) Then
+                Results.Items.Add(item)
+            End If
+        Next
+
+    End Sub
+End Class
